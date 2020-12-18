a39366203c0cb75ada2014472e11aad506073810
Justin Bush <revblaze@users.noreply.github.com>
2020-12-18 18:10:52 -0500

    Test commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-async function helloWorld() {
+async function helloWorld2() {
   console.log("Hello world");
   console.log("Waiting 1 sec");
+
   await new Promise((r) => setTimeout(r, 1000));
   console.log("Done waiting");
 }
 
-helloWorld();
+helloWorld2();
