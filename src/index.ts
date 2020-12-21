634a0c43fbb6dc9c4cf0e863a2f7e8427cc07977
Justin Bush <revblaze@users.noreply.github.com>
2020-12-21 17:31:18 -0500

    ewrite TypeScript example file for Swift transpiling demo
    TODO
    • Add custom TS/linter config for type-safe window.webkit
    • Uncomment lines 24-27
    • Remove @ts-ignore

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,28 @@
-async function helloWorld2() {
-  console.log("Hello world");
-  console.log("Waiting 1 sec");
+async function toggleMode() {
+  const body = document.querySelector("body");
+
+  if (!body?.hasAttribute("mode")) {
+    body?.setAttribute("mode", "light");
+  }
+
+  const mode = body?.getAttribute("mode");
+
+  if (mode == "light") {
+    body?.style.setProperty("color", "#fff");
+    body?.style.setProperty("background", "#000");
+    body?.setAttribute("mode", "dark");
+  } else {
+    body?.style.setProperty("color", "#000");
+    body?.style.setProperty("background", "#fff");
+    body?.setAttribute("mode", "light");
+  }
 
   await new Promise((r) => setTimeout(r, 1000));
-  console.log("Done waiting");
-}
 
-helloWorld2();
+  const result = "Mode: " + body?.getAttribute("mode")?.toString();
+  /*
+  // @ts--ignore
+  window.webkit.messageHandlers.eventListeners.postMessage(result);
+  */
+  console.log(result);
+}
