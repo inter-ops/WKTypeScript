9e07342921fec1e96e5737b78cd1a30be64a33cd
Justin Bush <revblaze@users.noreply.github.com>
2020-12-21 15:06:13 -0500

    Add WindowController, implement ViewController

--- Example/macOS/Example/ViewController.swift
+++ Example/macOS/Example/ViewController.swift
@@ -10,14 +10,28 @@ import WebKit
 
 class ViewController: NSViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
     
+    @IBOutlet var webView: WKWebView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        initWebView()
+        
+    }
+    
+    // MARK: WebKit Config
+    /// Initializes the main WKWebView object.
+    func initWebView() {
+        webView.uiDelegate = self                           // Set WebView UI Delegate
+        webView.navigationDelegate = self                   // Set WebView Navigation Delegate
+        // JavaScript Event Listeners
+        webView.configuration.userContentController.add(self, name: "eventListeners")
+        // Load URL
+        webView.load("https://google.com")
     }
     
     
+    // MARK: JavaScript Handler
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         // JavaScript event listeners
     }
---
+++ Example/macOS/Example/WindowController.swift
@@ -0,0 +1,18 @@
+//
+//  WindowController.swift
+//  Example
+//
+//  Created by Justin Bush on 2020-12-21.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+
+}
