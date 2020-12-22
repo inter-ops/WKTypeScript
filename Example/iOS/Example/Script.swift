8e03a09bff8e0f68e093866bcc114c2d483b1670
Justin Bush <revblaze@users.noreply.github.com>
2020-12-22 11:34:05 -0500

    Implement ViewController, add type-safe Script struct

---
+++ Example/iOS/Example/Script.swift
@@ -0,0 +1,14 @@
+//
+//  Script.swift
+//  Example
+//
+//  Created by Justin Bush on 2020-12-22.
+//
+
+import Foundation
+
+struct Script {
+    
+    static let toggleMode = "toggleMode();"
+    
+}
