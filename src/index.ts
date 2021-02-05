add4f00baa1b6d0919980c26a615354362ad3323
Justin Bush <revblaze@users.noreply.github.com>
2021-02-05 17:08:40 -0500

    Implement new index.ts demo

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,58 @@
+function postMessage(msg: string) {
+  window.webkit.messageHandlers.eventListeners.postMessage(`${msg}`);
+}
+
+function toggle() {
+  postMessage(`Toggle`);
+}
+
+function setLabel(text: string) {
+  postMessage(`[setLabel] ${text}`);
+  return text;
+}
+
+function hideLabel(hidden = false) {
+  postMessage(`[hideLabel] ${hidden}`);
+  return hidden;
+}
+
+function addNumbers(a: number, b: number) {
+  const sum = a + b;
+  postMessage(`[addNumbers] ${sum}`);
+}
+
+enum Device {
+  Phone = "iPhone",
+  Pad = "iPad",
+  Mac = "macOS"
+}
+function selectDevice(device: Device) {
+  const deviceString = device.toString();
+  postMessage(`[selectDevice] ${deviceString}`);
+  return deviceString;
+}
+
+enum Mode {
+  Light = "light",
+  Dark = "dark"
+}
+function setMode(mode: Mode = Mode.Light) {
+  let modeCase = "";
+  if (mode === Mode.Light) {
+    modeCase = "Mode.Light";
+  } else {
+    modeCase = `Mode.Dark`;
+  }
+  postMessage(`[setMode] mode: ${modeCase}, setting to ${mode.toString()} mode.`);
+  return mode;
+}
+
 async function toggleMode() {
   const body = document.querySelector("body");
-
   if (!body?.hasAttribute("mode")) {
     body?.setAttribute("mode", "light");
   }
-
   const mode = body?.getAttribute("mode");
-
   if (mode == "light") {
     body?.style.setProperty("color", "#fff");
     body?.style.setProperty("background", "#000");
@@ -16,12 +62,7 @@ async function toggleMode() {
     body?.style.setProperty("background", "#fff");
     body?.setAttribute("mode", "light");
   }
-
   await new Promise((r) => setTimeout(r, 1000));
-
   const result = "Mode: " + body?.getAttribute("mode")?.toString();
-  console.log(result);
-
-  // `wekbit` typing comes from `src/types/global.d.ts`
   window.webkit.messageHandlers.eventListeners.postMessage(result);
 }
