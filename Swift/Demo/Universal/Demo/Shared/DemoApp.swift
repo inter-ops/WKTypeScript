421b9af3aadcacc0be68f13b9b35bea2c5d88bc2
Justin Bush <revblaze@users.noreply.github.com>
2021-02-10 13:17:23 -0500

    Add Universal(SwiftUI) app package

---
+++ Swift/Demo/Universal/Demo/Shared/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Shared
+//
+//  Created by Justin Bush on 2021-02-09.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
