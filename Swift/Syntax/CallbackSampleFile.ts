f9c3833396f6e57713580fec3a3138287155868b
Justin Bush <revblaze@users.noreply.github.com>
2021-02-19 13:46:39 -0500

    Add sample file for Callbacks, create main GUIDE for cescyV

---
+++ Swift/Syntax/CallbackSampleFile.ts
@@ -0,0 +1,39 @@
+function returnBool() {
+  return true;
+}
+
+function returnString() {
+  return "Hello";
+}
+
+function returnNumber() {
+  return 14;
+}
+
+function returnBoolArray() {
+  return [true, false, false, true];
+}
+
+function returnStringArray() {
+  return ["John", "Male", "35"];
+}
+
+function returnNumberArray() {
+  return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+}
+
+function postMessageTest() {
+  window.webkit.messageHandlers.eventListeners.postMessage(`returnBool: ${returnBool()}`);
+  window.webkit.messageHandlers.eventListeners.postMessage(`returnString: ${returnString()}`);
+  window.webkit.messageHandlers.eventListeners.postMessage(`returnNumber: ${returnNumber()}`);
+}
+
+function postMessageTestArray() {
+  window.webkit.messageHandlers.eventListeners.postMessage(`returnBoolArray: ${returnBool()}`);
+  window.webkit.messageHandlers.eventListeners.postMessage(
+    `returnStringArray: ${returnStringArray()}`
+  );
+  window.webkit.messageHandlers.eventListeners.postMessage(
+    `returnNumberArray: ${returnNumberArray()}`
+  );
+}
